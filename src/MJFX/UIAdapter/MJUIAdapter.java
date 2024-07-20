bd554ea12c85a7c141ef450ff77695dbcef412c6
boss6731 <boss670731@gmail.com>
2024-07-21 01:03:12 +0800

    Update MJUIAdapter.java

--- src/MJFX/UIAdapter/MJUIAdapter.java
+++ src/MJFX/UIAdapter/MJUIAdapter.java
@@ -1,216 +1,132 @@
- package MJFX.UIAdapter;
+package MJFX.UIAdapter;
 
- import MJFX.Letter.MJFxLetterManager;
- import MJFX.Logger.MJFxLogger;
- import MJFX.MJFxEntry;
- import java.sql.Timestamp;
- import l1j.server.MJWebServer.Dispatcher.my.service.gmtools.MJMyGmService;
- import l1j.server.server.GameClient;
- import l1j.server.server.model.Instance.L1PcInstance;
- import l1j.server.server.model.L1World;
- import l1j.server.swing.chocco;
+import MJFX.Logger.MJFxLogger;
+import MJFX.MJFxEntry;
+import l1j.server.swing.chocco;
 
- public class MJUIAdapter
- {
-   private static boolean is_mjfx() {
-     return (MJFxEntry.getInstance() != null);
-   }
+public class MJUIAdapter {
 
-   public static void on_exit() {
-     if (is_mjfx()) {
-       MJFxEntry.getInstance().invoke_exit();
-     } else {
-       System.exit(0);
-     }
-   }
+	// 檢查是否在 MJFx 環境中運行
+	private static boolean is_mjfx() {
+		return (MJFxEntry.getInstance() != null);
+	}
 
-   public static void on_delete_letter(int id) {
-     if (is_mjfx()) {
-       MJFxLetterManager.getInstance().on_delete_letter(id);
-     }
-   }
+	// 處理添加交易聊天信息的方法
+	public static void on_chat_trade_append(String s) {
+		if (is_mjfx()) {
+			// 目前這裡沒有任何操作，可以根據需求添加
+		}
+	}
 
+	// 處理添加交易信息的方法
+	public static void on_trade_append(String s) {
+		if (is_mjfx()) {
+			// 在 MJFx 環境中，更新交易日誌
+			MJFxLogger.TRADE.update_log(s);
+		} else {
+			// 在非 MJFx 環境中，追加交易信息到相應的控件
+			chocco.txtTrade.append(s);
+		}
+	}
 
+	// 處理添加倉庫信息的方法
+	public static void on_warehouse_append(String s) {
+		if (is_mjfx()) {
+			// 在 MJFx 環境中，更新倉庫日誌
+			MJFxLogger.WAREHOUSE.update_log(s);
+		} else {
+			// 在非 MJFx 環境中，追加倉庫信息到相應的控件
+			chocco.txtWarehouse.append(s);
+		}
+	}
 
-   public static void on_check_letter(int id) {
-     if (is_mjfx()) {
-       MJFxLetterManager.getInstance().on_check_letter(id);
-     }
-   }
+	// 處理添加強化信息的方法
+	public static void on_enchant_append(String s) {
+		if (is_mjfx()) {
+			// 在 MJFx 環境中，更新強化監控日誌
+			MJFxLogger.ENCHANT_MONITOR.update_log(s);
+		} else {
+			// 在非 MJFx 環境中，追加強化信息到相應的控件
+			chocco.txtEnchant.append(s);
+		}
+	}
 
+	// 處理添加物品拾取信息的方法
+	public static void on_item_append(String s) {
+		if (is_mjfx()) {
+			// 在 MJFx 環境中，更新物品日誌
+			MJFxLogger.ITEM.update_log(s);
+		} else {
+			// 在非 MJFx 環境中，追加物品拾取信息到相應的控件
+			chocco.txtPickup.append(s);
+		}
+	}
 
-
-   public static void on_receive_letter(int id, String sender, String title, String content, Timestamp ts, String receiver) {
-     if (is_mjfx()) {
-       MJFxLetterManager.getInstance().on_receive_letter(id, sender, title, content, ts, receiver);
-     }
-   }
-
-
-
-   public static void on_login_user(GameClient clnt, L1PcInstance pc) {
-     if (is_mjfx()) {
-       MJFxLogger.LOGIN_CHARACTER.append_log(String.format("<登入> (%d)%s -%s", new Object[] { Integer.valueOf(L1World.getInstance().get_player_size()), pc.getName(), (clnt == null) ? "" : clnt.getHostname() }));
-     } else {
-       try {
-         chocco.count++;
-         chocco.label2.setText(" " + chocco.count);
-         chocco.userlist.add(pc.getName());
-       } catch (Exception exception) {}
-     }
-     MJMyGmService.service().onEnterCharacter(pc);
-   }
-
-   public static void on_logout_user(GameClient clnt, L1PcInstance pc) {
-     if (is_mjfx()) {
-       MJFxLogger.LOGIN_CHARACTER.append_log(String.format("<退出> (%d)%s -%s", new Object[] { Integer.valueOf(L1World.getInstance().get_player_size()), pc.getName(), (clnt == null) ? "" : clnt.getHostname() }));
-     } else {
-       try {
-         chocco.count--;
-         chocco.label2.setText(" " + chocco.count);
-         chocco.userlist.remove(pc.getName());
-       } catch (Exception exception) {}
-     }
-     MJMyGmService.service().onRemoveCharacter(pc);
-   }
-
-   public static void on_banip_clear() {
-     if (!is_mjfx())
-     {
-       chocco.iplist.removeAll();
-     }
-   }
-
-   public static void on_banip_append(String ip) {
-     if (!is_mjfx())
-     {
-       chocco.iplist.add(ip);
-     }
-   }
-
-   public static void on_shop_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.TRADE.update_log(s);
-     } else {
-       chocco.txtShop.append(s);
-     }
-   }
-   public static void on_chat_world_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.CHAT_WORLD.update_log(s);
-     } else {
-       chocco.txtGlobalChat.append(s);
-     }
-   }
-   public static void on_chat_normal_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.CHAT_NORMAL.update_log(s);
-     }
-   }
-
-
-   public static void on_chat_pledge_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.CHAT_PLEDGE.update_log(s);
-     } else {
-       chocco.txtClanChat.append(s);
-     }
-   }
-   public static void on_chat_party_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.CHAT_PARTY.update_log(s);
-     } else {
-       chocco.txtPartyChat.append(s);
-     }
-   }
-   public static void on_chat_whisper_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.CHAT_WHISPER.update_log(s);
-     } else {
-       chocco.txtWhisper.append(s);
-     }
-   }
-   public static void on_chat_trade_append(String s) {
-     if (is_mjfx());
-   }
-
-
-   public static void on_trade_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.TRADE.update_log(s);
-     } else {
-       chocco.txtTrade.append(s);
-     }
-   }
-   public static void on_warehouse_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.WAREHOUSE.update_log(s);
-     } else {
-       chocco.txtWarehouse.append(s);
-     }
-   }
-   public static void on_enchant_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.ENCHANT_MONITOR.update_log(s);
-     } else {
-       chocco.txtEnchant.append(s);
-     }
-   }
-   public static void on_item_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.ITEM.update_log(s);
-     } else {
-       chocco.txtPickup.append(s);
-     }
-   }
-
-   public static void on_create_account(String account, String hostname) {
-     if (is_mjfx()) {
-       MJFxLogger.ACCOUNT_CREATE.append_log(String.format("<建立帳號> %s -%s", new Object[] { account, hostname }));
-     }
-   }
-
-
-   public static void on_gm_command(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.GM_COMMAND.update_log(s);
-     }
-   }
-
-   public static void on_gm_command_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.GM_COMMAND.append_log(String.format("<GM指令結果>%s", new Object[] { s }));
-     } else {
-       System.out.println(s);
-     }
-   }
-   public static void on_minigame_append(String s) {
-     if (is_mjfx()) {
-       MJFxLogger.MINIGAME.append_log(s);
-     }
-   }
+	// 處理建立帳號的方法
+	public static void on_create_account(String account, String hostname) {
+		if (is_mjfx()) {
+			// 在 MJFx 環境中，記錄建立帳號信息
+			MJFxLogger.ACCOUNT_CREATE.append_log(
+					String.format("<建立帳號> %s -%s", new Object[]{ account, hostname })
+			);
+		}
+	}
 
 
 
-   public static void on_boss_append(int npc_id, String boss_name, int x, int y, int map_id) {
-     if (is_mjfx()) {
-       MJFxLogger.BOSS_TIMER.append_log(String.format("<老闆通知> %s(%d) %d,%d,%d", new Object[] { boss_name, Integer.valueOf(npc_id), Integer.valueOf(x), Integer.valueOf(y), Integer.valueOf(map_id) }));
-     }
-   }
+	public class MJUIAdapter {
 
-   public static void on_flush() {
-     if (!is_mjfx()) {
-       chocco.txtGlobalChat.setText("");
-       chocco.txtClanChat.setText("");
-       chocco.txtPartyChat.setText("");
-       chocco.txtWhisper.setText("");
-       chocco.txtShop.setText("");
-       chocco.txtTrade.setText("");
-       chocco.txtWarehouse.setText("");
-       chocco.txtEnchant.setText("");
-       chocco.txtPickup.setText("");
-     }
-   }
- }
+		// 檢查是否在 MJFx 環境中運行
+		private static boolean is_mjfx() {
+			return (MJFxEntry.getInstance() != null);
+		}
+
+		// 處理 GM 指令的方法
+		public static void on_gm_command(String s) {
+			if (is_mjfx()) {
+				MJFxLogger.GM_COMMAND.update_log(s);
+			}
+		}
+
+		// 處理追加 GM 指令結果的方法
+		public static void on_gm_command_append(String s) {
+			if (is_mjfx()) {
+				MJFxLogger.GM_COMMAND.append_log(String.format("<GM指令結果>%s", new Object[]{s}));
+			} else {
+				System.out.println(s);
+			}
+		}
+
+		// 處理迷你遊戲信息的方法
+		public static void on_minigame_append(String s) {
+			if (is_mjfx()) {
+				MJFxLogger.MINIGAME.append_log(s);
+			}
+		}
+
+		// 處理老闆通知的方法
+		public static void on_boss_append(int npc_id, String boss_name, int x, int y, int map_id) {
+			if (is_mjfx()) {
+				MJFxLogger.BOSS_TIMER.append_log(
+						String.format("<老闆通知> %s(%d) %d,%d,%d", new Object[]{boss_name, npc_id, x, y, map_id})
+				);
+			}
+		}
+
+		// 清除所有聊天和日誌訊息的方法
+		public static void on_flush() {
+			if (!is_mjfx()) {
+				chocco.txtGlobalChat.setText("");
+				chocco.txtClanChat.setText("");
+				chocco.txtPartyChat.setText("");
+				chocco.txtWhisper.setText("");
+				chocco.txtShop.setText("");
+				chocco.txtTrade.setText("");
+				chocco.txtWarehouse.setText("");
+				chocco.txtEnchant.setText("");
+				chocco.txtPickup.setText("");
+			}
+		}
+	}
 
 
