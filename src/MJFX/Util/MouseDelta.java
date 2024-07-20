0070880c510617137020b1050e46a4eb9d82648d
boss6731 <boss670731@gmail.com>
2024-07-20 23:54:25 +0800

    Update MouseDelta.java

--- src/MJFX/Util/MouseDelta.java
+++ src/MJFX/Util/MouseDelta.java
@@ -1,9 +1,28 @@
 package MJFX.Util;
 
 public class MouseDelta {
-  public double x;
-  
-  public double y;
+	// 滑鼠在 X 軸上的變化量
+	public double x;
+
+	// 滑鼠在 Y 軸上的變化量
+	public double y;
 }
+
+/*若需擴展功能，可以考慮添加構造函數或方法來初始化和操作這些變量，但目前這個類已經能夠滿足基本需求。例如，您可以添加一個構造函數來初始化 x 和 y：*/
+/*public class MouseDelta {
+	public double x;
+	
+	public double y;
+
+	// 構造函數來初始化 x 和 y
+	public MouseDelta(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}*/
+
+
+
+
 
 
